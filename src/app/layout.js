8ff04b5fab50af2d76a495defbaa2886dd56b58f
Gino Murin <gmurin@gmail.com>
2025-06-13 07:56:45 -0400

    added google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
@@ -66,10 +67,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TMR8WDHB');`
+        }} />
         {/* Emergency Phone Number for Click-to-Call */}
         <meta name="format-detection" content="telephone=yes" />
       </head>
       <body className={`${inter.className} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TMR8WDHB"
+            height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
+        </noscript>
         <StructuredData />
         <div className="min-h-screen flex flex-col">
           <Header />
